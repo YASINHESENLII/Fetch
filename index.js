a93c09c258fd107bf5178127227e17e4701f52ac
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-01 22:50:59 +0400

    adress++

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ function getJSONData(data) {
     var productcompanyNameTd = document.createElement("td");
     var productcontactNameTd = document.createElement("td");
     var productcontactTitleTd = document.createElement("td");
+    var productaddressTd= document.createElement("td");
+
     productcompanyNameTd.textContent = data[i].companyName;
+
     productcontactNameTd.textContent = data[i].contactName;
+
     productcontactTitleTd.textContent = data[i].contactTitle;
-    productRaw.append(productcompanyNameTd ,productcontactNameTd ,productcontactTitleTd);
+
+    
+    productaddressTd.textContent = data[i].address.street + ", " + data[i].address.country;
+
+    productRaw.append(productcompanyNameTd ,productcontactNameTd ,productcontactTitleTd,productaddressTd);
     akula.append(productRaw);
-  }
+ }
 }
